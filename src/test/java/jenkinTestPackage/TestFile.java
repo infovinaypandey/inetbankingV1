81a463ec976b2afb73e5f22bf6b003d3d2014eaf
vinaypandeycloudfx <vinay.pandey@cloudfx.com>
2019-11-05 19:16:58 +0530

    testing jenkins testng. xml

--- src/test/java/jenkinTestPackage/TestFile.java
+++ src/test/java/jenkinTestPackage/TestFile.java
@@ -6,6 +6,7 @@ public class TestFile {
 		System.out.println("This is test for time 05:00");
 		System.out.println("This is test for time 05:19");
 		System.out.println("This is test for time 05:30");
+		System.out.println("This is test for time 07:30");
 
 
 	}
